098084a83e9f22a7e910218494c7f10f0a344eb4
Will Acton <will@willacton.com>
2017-12-23 11:17:46 -0800

    Allow testing of previous days

--- test.js
+++ test.js
@@ -9,6 +9,19 @@ function nullableRequire(path) {
 }
 
 const dateMap = {
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine",
+    "10": "Ten",
+    "11": "Eleven",
+    "12": "Twelve",
+    "13": "Thirteen",
     "14": "Fourteen",
     "15": "Fifteen",
     "16": "Sixteen",
@@ -38,7 +51,6 @@ function runTest(file) {
   const capitalizedName = moduleName[0].toUpperCase() + moduleName.slice(1);
     const testPath = `./src/tests/${moduleName}Test.bs`;
     const testModule = require(testPath);
-    console.log(testModule);
     if (testModule) {
       console.log(`-----------------------------------------`);
       console.log(`Running tests for ${capitalizedName}...`)
